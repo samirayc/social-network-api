5b03dedf5c13236719f24f14a8a162323c6c8b01
Samira Chetta <chettasamira@gmail.com>
2024-03-17 17:42:39 -0400

    Function in thoughtControllers to get all thoughts/single thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,10 +2,27 @@ const { model } = require('mongoose');
 const { User, Thought } = require('../models');
 
 module.exports = {
+
     // Get all thoughts
+    getThought(req, res) {
+        Thought.find({})
+          .then((thought) => res.json(thought))
+          .catch((err) => res.status(500).json(err));
+    },
 
     // Get single thought
 
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+          .select("-__v")
+          .then((thought) =>
+            !thought
+              ? res.status(404).json({ message: "No Thought find with this ID!" })
+              : res.json(thought)
+          )
+          .catch((err) => res.status(500).json(err));
+      },
+      
     // Create a thought
 
     // Update a thought
