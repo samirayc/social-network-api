0b905c0b128d044666cbe34fb8f67580ea18a835
Samira Chetta <chettasamira@gmail.com>
2024-03-17 17:18:15 -0400

    Function to create/update/delete a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,13 +22,42 @@ module.exports = {
           )
           .catch((err) => res.status(500).json(err));
       },
-      
+
     // Create a user
+    createUser(req, res) {
+        User.create(req.body)
+          .then((user) => res.json(user))
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
 
     // Update a user
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+          { _id: req.params.userId },
+          { $set: req.body },
+          { runValidators: true, new: true }
+        )
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: "No User find with this ID!" })
+              : res.json(user)
+          )
+          .catch((err) => res.status(500).json(err));
 
     // Delete a user
     // BONUS: Remove a user's associated thoughts when deleted.
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: "No User find with this ID!" })
+              : Thought.deleteMany({ _id: { $in: user.thoughts } })
+          )
+          .then(() => res.json({ message: "User and Thought deleted!" }))
+          .catch((err) => res.status(500).json(err));
+      },
 
     // Add a friend
 
